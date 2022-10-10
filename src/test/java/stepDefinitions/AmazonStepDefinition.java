680333544df8b6033e19f8cee54155b79a5ea2ae
fatihhhh <fatihisiteknik42@gmail.com>
2022-10-10 20:59:36 +0300

    v06

--- src/test/java/stepDefinitions/AmazonStepDefinition.java
+++ src/test/java/stepDefinitions/AmazonStepDefinition.java
@@ -70,7 +70,7 @@ public class AmazonStepDefinition {
     }
 
     @And("sonuclarin {string} icerdigini test eder")
-    public void sonuclarinIcerdiginiTestEder(String istenenKelime) {234
+    public void sonuclarinIcerdiginiTestEder(String istenenKelime) {2345
 
         String actualText = amazonPage.aramaSonucuElementi.getText();
         String expectedText = istenenKelime;
